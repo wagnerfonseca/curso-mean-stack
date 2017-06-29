32c9c858a6972021289225e8e92586a267824d6c
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-06-28 21:01:01 -0300

    configurações iniciais para projeto backend NodeJs

---
+++ backend/config/server.js
@@ -0,0 +1,18 @@
+const PORT = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server =  express()
+
+
+/**
+ * urlEnconded é o formado dos dados do formulario: extends :true é para ser capaz de interpretar mais dados
+ * 
+ * use = para todas a requição que chegar para o meu servidor vai passar pode esse middleware
+ */
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(PORT, function() {
+    console.log(`BACKEND is running on port ${PORT}.`)
+})
