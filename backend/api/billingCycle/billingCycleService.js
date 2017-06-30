6e8a28c258fd69903369423951639b35dbf7f9c5
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-06-29 22:11:05 -0300

    configurações para o update e mais mensagens customizadas de erros

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -3,4 +3,8 @@ const BillingCycle = require('./billingCycle')
 // o plugin 'node-restful' vai criar todos os métodos dentro do padrão RESTFul
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 
+//no momento do update deve retorna o objeto com os novos dados atualizados
+// runValidators: para executar as validações no momento do update também
+BillingCycle.updateOptions({new: true, runValidators: true})
+
 module.exports = BillingCycle
--- backend/config/database.js
+++ backend/config/database.js
@@ -5,3 +5,6 @@ module.exports = mongoose.connect('mongodb://localhost/db_finance')
 
 // Mensagem padrão de erro para todos os elementos do model que são obrigatório
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = "O valor '{VALUE}' informado é menor que o limite mínimo de '{MIN}'"
+mongoose.Error.messages.Number.max = "O valor '{VALUE}' informado é maoir que o limite máximo de '{MAX}'"
+//mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
